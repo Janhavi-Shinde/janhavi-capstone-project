3dcb498bad684d73b9799cab660a922cc8aa72c2
janhavi <janashinde394@gmail.com>
2022-07-30 20:08:34 +1000

    added assoc and validations for all other models

--- app/models/sphere.rb
+++ app/models/sphere.rb
@@ -1,2 +1,8 @@
 class Sphere < ApplicationRecord
+    has_many :spheres_goals
+    has_many :goals, through: :spheres_goals
+    belongs_to :user
+
+    validates :title, presence: true
+    validates :title, uniqueness: true
 end
--- app/models/spheres_goal.rb
+++ app/models/spheres_goal.rb
@@ -1,2 +1,4 @@
 class SpheresGoal < ApplicationRecord
+    belongs_to :sphere
+    belongs_to :goal
 end

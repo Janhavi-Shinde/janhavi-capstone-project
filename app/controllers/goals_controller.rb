43fdcab4cca8d59691a88244473dec04929886de
janhavi <janashinde394@gmail.com>
2022-07-30 20:19:59 +1000

    added goal_Controller actions

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,70 +1,53 @@
 class GoalsController < ApplicationController
-  before_action :set_goal, only: %i[ show edit update destroy ]
-
-  # GET /goals or /goals.json
-  def index
-    @goals = Goal.all
-  end
-
-  # GET /goals/1 or /goals/1.json
-  def show
-  end
-
-  # GET /goals/new
   def new
-    @goal = Goal.new
+      @goal = Goal.new
+
+      @spheres = current_user.spheres.all
   end
 
-  # GET /goals/1/edit
-  def edit
-  end
-
-  # POST /goals or /goals.json
   def create
-    @goal = Goal.new(goal_params)
-
-    respond_to do |format|
-      if @goal.save
-        format.html { redirect_to goal_url(@goal), notice: "Goal was successfully created." }
-        format.json { render :show, status: :created, location: @goal }
+      @goal = Goal.new(goals_params)
+      if @goal.valid?
+      @goal.save
+       redirect_to goal_path(@goal)
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @goal.errors, status: :unprocessable_entity }
+          render :new
       end
-    end
   end
-
-  # PATCH/PUT /goals/1 or /goals/1.json
-  def update
-    respond_to do |format|
-      if @goal.update(goal_params)
-        format.html { redirect_to goal_url(@goal), notice: "Goal was successfully updated." }
-        format.json { render :show, status: :ok, location: @goal }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @goal.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /goals/1 or /goals/1.json
   def destroy
-    @goal.destroy
-
-    respond_to do |format|
-      format.html { redirect_to goals_url, notice: "Goal was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      @goal = Goal.find(params[:id])
+      @goal.destroy
+    
+      sphere_id = SpheresGoal.find_by(goal_id: @goal.id).sphere_id
+      redirect_to sphere_path(sphere_id) 
+       
+  end
+  
+  def edit
+      @goal = Goal.find(params[:id])
+      @spheres = current_user.spheres.all
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_goal
+  def update
       @goal = Goal.find(params[:id])
-    end
+      if @goal.update(goals_params)
 
-    # Only allow a list of trusted parameters through.
-    def goal_params
-      params.require(:goal).permit(:title, :description)
-    end
+      sphere_id = SpheresGoal.find_by(goal_id: @goal.id).sphere_id
+      redirect_to sphere_path(sphere_id)
+      else
+          render :edit
+
+      end
+  end
+
+  def show
+      @goal = Goal.find(params[:id])
+      
+
+  end
+  private
+  def goals_params
+      params.require(:goal).permit(:title, :description, sphere_ids:[])
+  end
+
 end

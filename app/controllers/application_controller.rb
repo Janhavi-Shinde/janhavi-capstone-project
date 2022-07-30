69a1973a1e1b422dd1121cdd88ab00573bf65c10
janhavi <janashinde394@gmail.com>
2022-07-30 19:33:27 +1000

    updated application_controller actions according to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
-end
+    helper_method :current_user
+        
+    
+        def require_logged_in
+            redirect_to controller: 'sessions', action: 'new' unless current_user 
+          end
+    
+          def current_user
+            User.find(session[:user_id])
+        # session[:user_id] 
+            end
+    end
+    

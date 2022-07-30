41a36e683aaeadee2f62084d2b471a67214ddd52
janhavi <janashinde394@gmail.com>
2022-07-30 20:06:49 +1000

    added assoc and validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :spheres
+
+    validates :email, presence: true
+    validates :name, presence: true
+    validates :password, presence: true
 end
